6b753745a4a95ceb2c4045af8e44565afbf9d7cd
Shelby Doolittle <shelby.doolittle@agilquest.com>
2014-05-05 16:36:40 -0400

    implement basic evaluator

--- diylisp/evaluator.py
+++ diylisp/evaluator.py
@@ -16,4 +16,33 @@ in a day, after all.)
 
 def evaluate(ast, env):
     """Evaluate an Abstract Syntax Tree in the specified environment."""
-    raise NotImplementedError("DIY")
+    if is_list(ast):
+    	operator = ast[0]
+    	left = evaluate(ast[1], env)
+    	if len(ast) > 2:
+    		right = evaluate(ast[2], env)
+
+    	if operator == "quote":
+    		return left
+    	elif operator == "atom":
+    		return is_atom(left)
+    	elif operator == "eq":
+    		if not is_atom(left) or not is_atom(right):
+    			return False
+    		return left == right
+    	elif operator in ["+", "-", "/", "*", "mod", ">"]:
+    		return evaluate_math(operator, left, right)
+    return ast
+
+def evaluate_math(operator, left, right):
+	try:
+		left = int(left)
+		right = int(right)
+		if operator == "+": return left + right
+		elif operator == "-": return left - right
+		elif operator == "/": return left / right
+		elif operator == "*": return left * right
+		elif operator == "mod": return left % right
+		elif operator == ">": return left > right
+	except:
+		raise LispError
