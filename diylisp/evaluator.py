d5e822dccb68cb13081ea9887db294232a2f8241
Shelby Doolittle <shelby@shelbyd.com>
2014-05-06 01:19:40 -0400

    implement lambdas

--- diylisp/evaluator.py
+++ diylisp/evaluator.py
@@ -19,6 +19,16 @@ def evaluate(ast, env):
     if is_list(ast):
         operator = ast[0]
 
+        if is_closure(operator):
+            params = dict()
+            if len(ast) != len(operator.params)+1:
+                raise LispError("wrong number of arguments, expected %d got %d" % (len(operator.params), len(ast)-1))
+            arg_index = 1
+            for param in operator.params:
+                params[param] = evaluate(ast[arg_index], env)
+                arg_index += 1
+            return evaluate(operator.body, operator.env.extend(params)) 
+
         # Part that does not evaluate arguments necessarily
         if operator == "quote":
             return ast[1] 
@@ -35,6 +45,14 @@ def evaluate(ast, env):
                     raise LispError("non-symbol")
             else:
                 raise LispError("Wrong number of arguments")
+        elif operator == "lambda":
+            if is_list(ast[1]):
+                if len(ast) == 3:
+                    return Closure(env, ast[1], ast[2]) 
+                else:
+                    raise LispError("number of arguments")
+            else:
+                raise LispError
 
         # Part that always evaluates arguments
         evaled = map(lambda expr: evaluate(expr, env), ast[1:])
@@ -46,6 +64,16 @@ def evaluate(ast, env):
             return evaled[0] ==evaled[1] 
         elif operator in ["+", "-", "/", "*", "mod", ">"]:
             return evaluate_math(operator, evaled[0], evaled[1])
+        else:
+            if is_symbol(operator):
+                # operator is possibly a function name
+                block = [env.lookup(ast[0])]
+            elif is_closure(evaluate(operator, env)):
+                block = [evaluate(operator, env)]
+            else:
+                raise LispError("not a function")
+            block.extend(ast[1:])
+            return evaluate(block, env)
 
     try:
         return env.lookup(ast)
